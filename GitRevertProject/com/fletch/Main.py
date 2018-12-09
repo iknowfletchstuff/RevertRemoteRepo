c3d1be45e881b3a4ba63c5a6dfd9b89fa13a74a0
Robert fletcher <iknowfletchstuff@yahoo.com>
2018-12-09 15:11:30 -0500

    Commit 1

---
+++ GitRevertProject/com/fletch/Main.py
@@ -0,0 +1,26 @@
+'''
+Created on Dec 9, 2018
+
+@author: fleth
+'''
+
+class Main(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        pass
+        '''
+        Constructor
+        '''
+    def execute(self):
+        print("Hello world")
+        
+        
+        
+if __name__ == '__main__':
+    main = Main()
+    main.execute()
+        
